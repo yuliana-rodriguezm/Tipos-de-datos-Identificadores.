768d68127d8f09c9383774aed3abb8642a54361f
yuliana-rodriguezm <ye.rodriguezm@uea.edu.ec>
2026-01-07 16:34:05 -0500

    Update conversor_temperatura.py
    Agregando nuevos comentarios claves para mejor entendimiento

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -5,18 +5,20 @@
 dia_mes = int(input("Ingrese el día del mes (1 al 31): "))
 print("Día del mes:", dia_mes)
 
-#Pide la temperatura, uso el tipo de dato float
+#Pide la temperatura, usando el tipo de dato float
 temperatura_celsius = float(input("Ingrese la temperatura en grados Celsius: "))
 
+# Transforma la temperatura de grados Celcius a Fahrenheit
 temperatura_fahrenheit = (temperatura_celsius * 9 / 5) + 32
 
 # Identifica si hace frío o calor según la temperatura
-# Uso el tipo de dato boolean
+# Uso del tipo de dato boolean
 es_caliente = temperatura_celsius >= 25
+print("¿Es caliente?:", es_caliente)
 
 # Muestra los resultados con el tipo de dato string
-print("Temperatura ingresada:", temperatura_celsius, "°C")
-print("Temperatura convertida:", temperatura_fahrenheit, "°F")
+print("Temperatura ingresada en celcius:", temperatura_celsius, "°C")
+print("Temperatura convertida a Fahrenheit:", temperatura_fahrenheit, "°F")
 
 if es_caliente:
     print("El clima es caliente.")
